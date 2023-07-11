383efebfc217e32d4731e2c956bb4967afcc08ba
SoumiaEl <Soumia.El_Montaser@etu.sorbonne-universite.fr>
2023-07-11 15:33:15 +0900

    Publisher Corrections: problem conversions

--- Assets/Scripts/HandleROSPosition.cs
+++ Assets/Scripts/HandleROSPosition.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using RosMessageTypes.Geometry;
 using RosMessageTypes.Sensor;
 using Unity.Robotics.ROSTCPConnector;
-using Unity.Robotics.UrdfImporter;
 using RosMessageTypes.Std;
 
 
@@ -15,26 +14,152 @@ public class HandleROSPosition : MonoBehaviour
     ROSConnection ros;
     // Drag the virtual hand object here in the Unity editor
     public GameObject virtualHand;
+    public GameObject base_link;
+    public GameObject endEffector;
 
-    private string desired_ee_pose_topic = "ee_target_pose";
-    private string joint_states_topic = "franka/joint_states";
-    private List<UnityEngine.ArticulationBody> robot_joints;
-    private bool firstUpdate = true;
+    private string robotNamespace = "franka";
+    private string desired_ee_pose_topic;
+    private string desired_joint_states_topic;
+    private List<ArticulationBody> robot_joints;
+    private int nb_robots_joints = 8;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        ros = ROSConnection.GetOrCreateInstance();
-        robot_joints = new List<UnityEngine.ArticulationBody>(GetComponentsInChildren<UnityEngine.ArticulationBody>());
 
-        // Subscribe to joint states
-        ros.Subscribe<JointStateMsg>(joint_states_topic, HandleJointStateMessage);
+        ros = ROSConnection.GetOrCreateInstance();
+
+        desired_ee_pose_topic = "ee_target_pose";
+        desired_joint_states_topic = robotNamespace + "/desired_joint_state";
+
+        robot_joints = new List<ArticulationBody>(GetComponentsInChildren<ArticulationBody>());
+
+        Debug.Log(robot_joints.Count);
+
+        ros.RegisterPublisher<JointStateMsg>(robotNamespace + "/current_joint_states");
+        ros.RegisterPublisher<PoseStampedMsg>("ee_target_pose");
+
+        // Invoke these methods after a delay of 1 second.
+        Invoke("CurrentJointState", 1);
+        Invoke("SendTargetPose", 1);
+
+        Vector3 rosPosition = ConvertUnityToRosPositionCoordinate(virtualHand.transform.position);
+        Quaternion rosOrientation = ConvertUnityToRosOrientationCoordinate(virtualHand.transform.rotation);
+
+        Debug.Log("Initial Position in ROS coordinates: " + rosPosition);
+        Debug.Log("Initial Orientation in ROS coordinates: " + rosOrientation);
+
+
     }
 
-    void HandleJointStateMessage(JointStateMsg msg)
+    void CurrentJointState()
     {
-        // Update the joint targets
-        for (int i = 0; i < robot_joints.Count; i++)
+
+        // Publish initial joint states
+
+        var jointStateMsg = new JointStateMsg
+        {
+            position = new double[nb_robots_joints],
+            name = new string[nb_robots_joints],
+            velocity = new double[nb_robots_joints],
+            effort = new double[nb_robots_joints]
+        };
+
+        jointStateMsg.header.frame_id = "panda_link0";
+
+
+        for (int i = 0; i < nb_robots_joints; i++)
+        {
+
+            jointStateMsg.name[i] = robot_joints[i].name;
+            jointStateMsg.velocity[i] = robot_joints[i].velocity[0];
+
+            //jointStateMsg.effort[i] = effort_for_joint[i];      // If you can get joint efforts
+
+            if (robot_joints[i].jointType == ArticulationJointType.FixedJoint)
+            {
+                jointStateMsg.position[i] = 0;
+            }
+            else
+            {
+                jointStateMsg.position[i] = robot_joints[i].jointPosition[0];
+            }
+            Debug.Log("Position du Joint" + i + ":" + jointStateMsg.position[i]);
+
+        }
+
+        Debug.Log(jointStateMsg);
+        ros.Publish(robotNamespace + "/current_joint_states", jointStateMsg);
+
+        Debug.Log("Test Position de l'effecteur :" + endEffector.transform.position);
+        Debug.Log("Test Rotation de l'effecteur :" + endEffector.transform.rotation);
+
+    }
+
+    void SendTargetPose()
+    {
+
+      
+        // Obtenir la position et l'orientation de la main virtuelle
+        Vector3 unityPosition = endEffector.transform.InverseTransformPoint(virtualHand.transform.position);
+        Quaternion unityOrientation = Quaternion.Inverse(endEffector.transform.rotation) * virtualHand.transform.rotation;
+
+        Quaternion rosOrientation = ConvertUnityToRosOrientationCoordinate(unityOrientation);
+        Vector3 rosPosition = ConvertUnityToRosPositionCoordinate(unityPosition);
+
+        // Convertir les coordonnées de Unity en coordonnées ROS
+        //rosPosition = ConvertUnityToRosPositionCoordinate(rosPosition);
+        //rosOrientation = ConvertUnityToRosOrientationCoordinate(rosOrientation);
+
+
+        // Créer un QuaternionMsg à partir de l'orientation ROS normalisée
+        QuaternionMsg normalizedQuaternionMsg = new QuaternionMsg
+        {
+            x = rosOrientation.x,
+            y = rosOrientation.y,
+            z = rosOrientation.z,
+            w = rosOrientation.w
+        };
+
+        // Créer un PoseMsg avec la position et l'orientation ROS
+        var poseMsg = new PoseMsg
+        {
+            position = new PointMsg
+            {
+                x = rosPosition.x,
+                y = rosPosition.y,
+                z = rosPosition.z
+            },
+            orientation = normalizedQuaternionMsg
+        };
+
+        // Créer un PoseStampedMsg et le publier
+        var poseStampedMsg = new PoseStampedMsg
+        {
+            header = new HeaderMsg { frame_id = "panda_link0" },
+            pose = poseMsg
+        };
+
+        Debug.Log("Position souhaitée : " + poseStampedMsg);
+
+        ros.Publish(desired_ee_pose_topic, poseStampedMsg);
+    }
+
+
+    void HandleDesiredJointStateMessage(JointStateMsg msg)
+    {
+        Debug.Log("Voici le message reçu sous ROS:" + msg);
+        // Vérifiez que le message a le bon nombre de positions
+        if (msg.position.Length != nb_robots_joints)
+        {
+            Debug.LogError("Received JointStateMsg with incorrect number of positions");
+            return;
+        }
+
+        // Mettez à jour les positions cibles pour chaque joint
+        for (int i = 1; i < nb_robots_joints; i++)
         {
             var drive = robot_joints[i].xDrive;
             drive.target = (float)msg.position[i];
@@ -42,54 +167,39 @@ public class HandleROSPosition : MonoBehaviour
         }
     }
 
+    Vector3 ConvertUnityToRosPositionCoordinate(Vector3 unityPosition)
+    {
+        return new Vector3(-unityPosition.z, -unityPosition.x, unityPosition.y);
+    }
+
+
+    Quaternion ConvertUnityToRosOrientationCoordinate(Quaternion unityQuaternion)
+    {
+        return new Quaternion(unityQuaternion.y, unityQuaternion.z, -unityQuaternion.x, -unityQuaternion.w);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if (firstUpdate)
-        {
-            for (int i = 0; i < robot_joints.Count; i++)
-            {
-                var drive = robot_joints[i].xDrive;
-                drive.target = 0.0f;  // Default position for each joint
-                robot_joints[i].xDrive = drive;
-            }
+        // Subscribe to joint states
+        ros.Subscribe<JointStateMsg>(desired_joint_states_topic, HandleDesiredJointStateMessage);
+        Debug.Log("Position de l'end effecteur:" + endEffector.transform.position);
 
-            firstUpdate = false;
-        }
-
-        // Publish end-effector pose
-        var poseMsg = new PoseMsg
-        {
-            position = new PointMsg 
-            {
-                x = virtualHand.transform.position.x,
-                y = virtualHand.transform.position.y,
-                z = virtualHand.transform.position.z
-            },
-            orientation = new QuaternionMsg 
-            {
-                x = virtualHand.transform.rotation.x,
-                y = virtualHand.transform.rotation.y,
-                z = virtualHand.transform.rotation.z,
-                w = virtualHand.transform.rotation.w
-            }
-        };
-
-        var poseStampedMsg = new PoseStampedMsg
-        { 
-            header = new HeaderMsg { frame_id = "base_link" },
-            pose = poseMsg
-        };
-
-        // Publish current joint states
-        var jointStateMsg = new JointStateMsg();
-        jointStateMsg.position = new double[robot_joints.Count];
-        for (int i = 0; i < robot_joints.Count; i++)
-        {
-            jointStateMsg.position[i] = robot_joints[i].xDrive.target;
-        }
-        
-        ros.Publish(joint_states_topic, jointStateMsg);
-        ros.Publish(desired_ee_pose_topic, poseStampedMsg);
     }
 }
+
+
+/*
+ Position 
+x : 0.156
+y: -0.263
+z : 0.29
+
+Rotation 
+x: 0.00027
+y : 0.00079,
+z  -1.00000 
+w : 0.00000
+
+ */
